971ee02eb17f70665e738cd3eccdaf92812486d5
Sean Leif Hellier <sean@newtropolis.com>
2020-03-29 04:44:22 -0400

    added minor changes to the skin

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./footer.css";
 
 function Footer() {
-    return (
-        <div>
-            <nav class="fixed-bottom footer navbar-light ">
-                <a class="footertext" href="https://github.com/Koalescere/deniXone">deniXone was created by: Gavin Martin, Sean Hellier, and Daniel Mayer</a>
-            </nav>
-        </div>
+    return ( <
+        div >
+        <
+        nav class = "fixed-bottom footer navbar-light " >
+        <
+        a class = "footertext"
+        href = "https://github.com/Koalescere/deniXone" > deniXone was created by: Gavin Martin, Sean Hellier, and Daniel Mayer < /a> <
+        /nav> <
+        /div>
     )
 }
 
